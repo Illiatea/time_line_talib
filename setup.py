97e6e1e24795e3742acd95c066693e676d274ee3
Illiatea <36513483+Illiatea@users.noreply.github.com>
2023-07-24 23:37:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='1.0',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
-    py_modules=['time_line_talib.time_line_source.indicators'],
     install_requires=['requests', 'TA-Lib']
 )
