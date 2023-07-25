83608b37c3130dc3e9be6a951bced123f9663e71
Vitche Research Team Developer <developer@vitche.com>
2023-07-25 20:06:56 +0300

    1. Added comments to the code.

--- time_line_source/indicators.py
+++ time_line_source/indicators.py
@@ -1,13 +1,23 @@
 import requests
 import json
 import talib
+# TODO: This is explained by the IDE
 import numpy as np
 
 
+# TODO: After other re-factoring, the class name will be more logical to become something like "TALibProcessor".
+# TODO: Other "processor" examples:
+# TODO: - https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/converters.py
+# TODO: - https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/filters.py
 class TimelineSourceIndicator:
+
+    # TODO: The constructor can create such object, which is logical:
+    #  https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/sources.py#L13
     def __init__(self, url):
         self.url = url
 
+    # TODO: Loading factual data is already available in the following library:
+    #  https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/sources.py#L33
     def get_data(self):
         response = requests.get(self.url)
         data_str = response.content.decode('utf-8')
@@ -20,6 +30,9 @@ class TimelineSourceIndicator:
             parsed_data.append({'time': time, 'value': value_json})
         return parsed_data
 
+    # TODO: All PySyun processor are organized as classes, which store parameters in the constructor and then just
+    #  accept the list of data in the method, called "process". This class should be the same,
+    #  after you change the constructor.
     def get_indicator(self, indicator_name, period):
         data = self.get_data()
         indicator_func = getattr(talib, indicator_name)
@@ -31,6 +44,8 @@ class TimelineSourceIndicator:
                 indicator_args.append(float(i))
                 true_data.append(d)
             except Exception as ex:
+                # TODO: This situation results in unpredicted consequences. Need to analyze, why this happens.
+                # TODO: And then, such errors are fixed using logical checks.
                 continue
         data = true_data
         indicator_args = np.array(indicator_args)
