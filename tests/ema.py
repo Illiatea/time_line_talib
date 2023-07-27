d1ac89297fa3c5d84bf0ef021210564bbf73a9d7
Illia Tsokolenko <illiatea2@gmail.com>
2023-07-27 16:00:16 +0300

    time_line_talib version 1.1

--- tests/ema.py
+++ tests/ema.py
@@ -1,7 +1,7 @@
-from time_line_source.indicators import TimelineSourceIndicator
+from time_line_source.indicators import TALibProcessor
 
 url = "https://europe-west1-hype-dev.cloudfunctions.net/storage-timeline-all?format=string&schema=ethereum.lovelyswap-v4.lovely.finance&timeLine=0x3aB9323992DFf9231D40E45C4AE009db1a35e40b"
-my_data = TimelineSourceIndicator(url)
+my_data = TALibProcessor(url)
 
 ema_data = my_data.get_indicator("EMA", 20)
 print(ema_data)
--- time_line_source/indicators.py
+++ time_line_source/indicators.py
@@ -1,54 +1,21 @@
-import requests
-import json
 import talib
-# TODO: This is explained by the IDE
-import numpy as np
+import numpy
 
+class TALibProcessor:
 
-# TODO: After other re-factoring, the class name will be more logical to become something like "TALibProcessor".
-# TODO: Other "processor" examples:
-# TODO: - https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/converters.py
-# TODO: - https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/filters.py
-class TimelineSourceIndicator:
+    def __init__(self, time_line):
+        self.time_line = time_line
 
-    # TODO: The constructor can create such object, which is logical:
-    #  https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/sources.py#L13
-    def __init__(self, url):
-        self.url = url
-
-    # TODO: Loading factual data is already available in the following library:
-    #  https://github.com/pysyun/pysyun-timeline/blob/master/pysyun/timeline/sources.py#L33
-    def get_data(self):
-        response = requests.get(self.url)
-        data_str = response.content.decode('utf-8')
-        data_json = json.loads(data_str)
-        parsed_data = []
-        for item in data_json:
-            time = item['time']
-            value_str = item['value']
-            value_json = json.loads(value_str)
-            parsed_data.append({'time': time, 'value': value_json})
-        return parsed_data
-
-    # TODO: All PySyun processor are organized as classes, which store parameters in the constructor and then just
-    #  accept the list of data in the method, called "process". This class should be the same,
-    #  after you change the constructor.
     def get_indicator(self, indicator_name, period):
-        data = self.get_data()
         indicator_func = getattr(talib, indicator_name)
         indicator_args = []
-        true_data = []
+        data = self.time_line
         for d in data:
-            try:
-                i = d['value']['r'][0]
-                indicator_args.append(float(i))
-                true_data.append(d)
-            except Exception as ex:
-                # TODO: This situation results in unpredicted consequences. Need to analyze, why this happens.
-                # TODO: And then, such errors are fixed using logical checks.
-                continue
-        data = true_data
-        indicator_args = np.array(indicator_args)
+            i = d['value']
+            print(i)
+            indicator_args.append(float(i))
+
+        indicator_args = numpy.array(indicator_args)
         indicator_result = indicator_func(indicator_args, timeperiod=period)
         result = [{'time': data[i]['time'], 'value': indicator_result[i]} for i in
                   range(len(data) - period + 1)]
