b4b568091643bc1766aff0e9320798839c2275e2
pysyun <48061047+pysyun@users.noreply.github.com>
2023-07-28 01:35:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_storage_timeline_talib',
-    version='1.2',
+    version='1.3',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
     install_requires=['TA-Lib'],
