95462f725cb491a4c54773e93b948604379110fc
Vitche Research Team Developer <developer@vitche.com>
2023-07-27 21:54:22 +0300

    1. The code review.

--- tests/rsi.py
+++ tests/rsi.py
@@ -1,6 +1,9 @@
 from time_line_source.indicators import TALibProcessor
 
-url = "https://europe-west1-hype-dev.cloudfunctions.net/storage-timeline-all?format=string&schema=ethereum.lovelyswap-v4.lovely.finance&timeLine=0x3aB9323992DFf9231D40E45C4AE009db1a35e40b"
+# TODO: Now tests do not match the new code. You can just save a JSON file locally and use it to avoid calling the
+#  database and linking to a database library.
+url = "https://europe-west1-hype-dev.cloudfunctions.net/storage-timeline-all?format=string&schema=ethereum.lovelyswap" \
+      "-v4.lovely.finance&timeLine=0x3aB9323992DFf9231D40E45C4AE009db1a35e40b"
 my_data = TALibProcessor(url)
 
 rsi_data = my_data.get_indicator("RSI", 14)
